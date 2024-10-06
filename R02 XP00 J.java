79ab10416fc604df95e78435b999a18f0905533d
myles1221 <70238169+myles1221@users.noreply.github.com>
2024-10-06 17:05:49 -0400

    Update R02 XP00 J.java
    fixes applied

--- R02 XP00 J.java
+++ R02 XP00 J.java
@@ -5,14 +5,19 @@ public void deleteFile(){
  
   File someFile = new File("someFileName.txt");
   // Do something with someFile
-  someFile.delete();
- 
+  if(!someFile.delete()){
+   //handle failure to delete the file
+  }
+
+ //It is especially important to process the return values of immutable object methods
+ //Although many methods of mutable objects operate by changing some internal state of the object, methods of immutable objects
+ // cannot change the object and often return a mutated new object, leaving the original object unchanged.
 }
 
 public class Replace {
   public static void main(String[] args) {
     String original = "insecure";
-    original.replace('i', '9');
+    original = original.replace('i', '9');
     System.out.println(original);
   }
 }
